f31196735f122ddd465bffa491335c16cb5f7d8f
Alex V <alexandervasse@gmail.com>
2022-11-10 17:01:41 +0900

    11/08

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Movie.create(title: "Inception", overview: "A skilled thief commits corporate espionage by infiltrating the subconscious of his targets.", poster_url: "https://image.tmdb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg", rating: 9.8)
+Movie.create(title: "Interstellar", overview: "A group of explorers travel and conquer the vast distances involved in an interstellar voyage.", poster_url: "https://image.tmdb.org/t/p/original/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg", rating: 8.2)
+Movie.create(title: "Tenet", overview: "The Protagonist journeys through a twilight world of international espionage", poster_url: "https://image.tmdb.org/t/p/original/oh8XmxWlySHgGLlx8QOBmq9k72j.jpg", rating: 9.2)
+Movie.create(title: "The Dark Knight", overview: "Batman raises the stakes in his war on crime", poster_url: "https://image.tmdb.org/t/p/original/qJ2tW6WMUDux911r6m7haRef0WH.jpg", rating: 8.9)
+
+
+
+
