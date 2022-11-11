54e569b75ea85e76feb0bf92bbef9ec06fc6f675
Alex V <alexandervasse@gmail.com>
2022-11-11 11:15:07 +0900

    11/11

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
+
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
